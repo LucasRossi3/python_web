ab2ff522846178862dcc34fa348b751f70d61825
Lucas Rossi <ljmr.rossi@gmail.com>
2023-02-14 23:42:27 +0000

    Criando 1° blog

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask("Hello")
+app = Flask(__name__)
+
+# POSTS MOCK
+posts = [
+    {
+        "titulo": "Post 1",
+        "texto": "Meu primeiro post"
+    }, 
+    {   
+        "titulo": "Post 2",
+        "texto": "Meu segundo post"
+    },
+    {   
+        "titulo": "Post 3",
+        "texto": "Meu terceiro post"
+    }
+]
 
 @app.route("/")
-def hello():
-    return "Hello World"
+def exibir_entradas():
+    return render_template('exibir_entradas.html', legal=posts)
+
+@app.route("/legal")
+def legal():
+    return "<h1>Voce é muito legal</h1>"
